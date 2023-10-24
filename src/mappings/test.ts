676824b58c201dcb9851ee063fb527f3d3bf659b
vutran1710 <me@vutr.io>
2023-10-24 15:23:10 +0700

    up

--- src/mappings/test.ts
+++ src/mappings/test.ts
@@ -1,9 +1,5 @@
 import { log } from '@graphprotocol/graph-ts'
 
-export function testAll(): void {
-  log.info('Hello world')
-}
-
 export function testLog(): void {
   log.debug(
     'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English',
@@ -29,4 +25,9 @@ export function testLog(): void {
 
 export function testBigIntPlus(): void {
   // implement
+}
+
+export function testAll(): void {
+  testLog()
+  testBigIntPlus()
 }
