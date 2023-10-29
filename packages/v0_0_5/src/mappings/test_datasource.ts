e8b4d033cde0ee90467411d333dd7d237373405b
vutran1710 <me@vutr.io>
2023-10-29 17:03:04 +0700

    Add datasource test

---
+++ packages/v0_0_5/src/mappings/test_datasource.ts
@@ -0,0 +1,9 @@
+import { ethereum, log } from '@graphprotocol/graph-ts'
+
+export function testHandlerEvent(event: ethereum.Event): void {
+  log.info('Received event: {}', [event.address.toHexString()])
+}
+
+export function testHandlerBlock(block: ethereum.Block): void {
+  log.info('Received block: {}, hash={}', [block.number.toString(), block.hash.toHexString()])
+}
