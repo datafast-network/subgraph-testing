ccf6349a3b457a329d53cedbbb7bb694a5c0ca87
vutran1710 <me@vutr.io>
2023-10-29 18:20:49 +0700

    update test mapping

--- packages/v0_0_4/src/mappings/test_datasource.ts
+++ packages/v0_0_4/src/mappings/test_datasource.ts
@@ -1,9 +1,18 @@
 import { ethereum, log } from '@graphprotocol/graph-ts'
 
 export function testHandlerEvent(event: ethereum.Event): void {
-  log.info("Received event: {}", [event.address.toHexString()]);
+  log.info('Received event: address={}, block={}', [event.address.toHexString(), event.block.number.toString()])
 }
 
 export function testHandlerBlock(block: ethereum.Block): void {
-  log.info("Received block: {}, hash={}", [block.number.toString(), block.hash.toHexString()]);
+  log.info('Received block: {}, hash={}', [block.number.toString(), block.hash.toHexString()])
 }
+
+export function testHandlerTransaction(tx: ethereum.Transaction): void {
+  log.info('Received tx: from={}, to={}, value={}', [tx.from.toHexString(), tx.to!.toHexString(), tx.value.toString()])
+}
+
+// v0_0_4 does not provide ethereum.Log
+// export function testHandlerLog(ethLog: ethereum.Log): void {
+//   log.info('Received log: data={}', [ethLog.data.toHexString()])
+// }
