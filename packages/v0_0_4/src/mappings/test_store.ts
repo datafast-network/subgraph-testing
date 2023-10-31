e90bb9bd7e3b7c3fc7fbc7d463f8a39a520dd607
vutran1710 <me@vutr.io>
2023-10-31 16:38:30 +0700

    update test

--- packages/v0_0_4/src/mappings/test_store.ts
+++ packages/v0_0_4/src/mappings/test_store.ts
@@ -4,10 +4,10 @@ import { ZERO_BI, ZERO_BD } from './../utils/constants'
 
 export function testStoreSet(): void {
   let token = new Token('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
-  token.symbol = fetchTokenSymbol(event.params.token)
-  token.name = fetchTokenName(event.params.token)
+  token.symbol = "MYCOIN"
+  token.name = "MyCoin"
   token.totalSupply = BigInt.fromString('1000000000000')
-  token.decimals = 10
+  token.decimals = BigInt.fromI32(10)
   token.derivedETH = ZERO_BD
   token.volume = ZERO_BD
   token.volumeUSD = ZERO_BD
@@ -24,5 +24,5 @@ export function testStoreSet(): void {
 
 export function testStoreGet(): Token {
   let token = Token.load('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
-  return token
+  return token!
 }
