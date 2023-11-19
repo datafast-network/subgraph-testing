4e90425a58b44b4e3caae0a60ff9ad8b480ab05a
quannguyen <quanna.dev@gmail.com>
2023-11-19 18:50:16 +0700

    add test call

---
+++ packages/v0_0_5/src/mappings/test_ethereum_call.ts
@@ -0,0 +1,11 @@
+import {Address, log} from "@graphprotocol/graph-ts";
+import {ERC20SymbolBytes} from "../types/Factory/ERC20SymbolBytes";
+import {ERC20} from "../types/ethereum_call/ERC20";
+
+
+export function testEthereumCall(): void {
+    let tokenAddress = Address.fromString('0x95a41fb80ca70306e9ecf4e51cea31bd18379c18')
+    let contractSymbolBytes = ERC20.bind(tokenAddress)
+    let symbolResultBytes = contractSymbolBytes.symbol();
+    log.info('symbolResultBytes={}', [symbolResultBytes])
+}
