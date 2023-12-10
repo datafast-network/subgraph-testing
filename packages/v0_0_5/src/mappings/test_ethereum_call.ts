6fcfad24da3f76db6d5081099ffa0abe21999d2b
quannguyen <quanna.dev@gmail.com>
2023-12-10 22:21:08 +0700

    fix build

--- packages/v0_0_5/src/mappings/test_ethereum_call.ts
+++ packages/v0_0_5/src/mappings/test_ethereum_call.ts
@@ -1,5 +1,5 @@
 import { Address, log } from '@graphprotocol/graph-ts'
-import { ERC20 } from '../types/ethereum_call/ERC20'
+import { ERC20 } from '../types/TestEthereumCall/ERC20'
 
 export function testEthereumCall(): void {
   let tokenAddress = Address.fromString('0x95a41fb80ca70306e9ecf4e51cea31bd18379c18')
