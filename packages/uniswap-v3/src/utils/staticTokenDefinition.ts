af77a9a2a38947df67c898a9712b39f516158916
vutran1710 <me@vutr.io>
2023-12-14 21:58:29 +0700

    fix array error

--- packages/uniswap-v3/src/utils/staticTokenDefinition.ts
+++ packages/uniswap-v3/src/utils/staticTokenDefinition.ts
@@ -1,11 +1,8 @@
-import {
-  Address,
-  BigInt,
-} from "@graphprotocol/graph-ts"
-  
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+
 // Initialize a Token Definition with the attributes
 export class StaticTokenDefinition {
-  address : Address
+  address: Address
   symbol: string
   name: string
   decimals: BigInt
@@ -19,8 +16,8 @@ export class StaticTokenDefinition {
   }
 
   // Get all tokens with a static defintion
-  static getStaticDefinitions(): Array<StaticTokenDefinition> {
-    let staticDefinitions = new Array<StaticTokenDefinition>(6)
+  static getStaticDefinitions(): Array<StaticTokenDefinition | null> {
+    let staticDefinitions = new Array<StaticTokenDefinition | null>(6)
 
     // Add DGD
     let tokenDGD = new StaticTokenDefinition(
@@ -80,14 +77,19 @@ export class StaticTokenDefinition {
   }
 
   // Helper for hardcoded tokens
-  static fromAddress(tokenAddress: Address) : StaticTokenDefinition | null {
+  static fromAddress(tokenAddress: Address): StaticTokenDefinition | null {
     let staticDefinitions = this.getStaticDefinitions()
     let tokenAddressHex = tokenAddress.toHexString()
 
     // Search the definition using the address
     for (let i = 0; i < staticDefinitions.length; i++) {
       let staticDefinition = staticDefinitions[i]
-      if(staticDefinition.address.toHexString() == tokenAddressHex) {
+
+      if (!staticDefinition) {
+        continue
+      }
+
+      if (staticDefinition.address.toHexString() == tokenAddressHex) {
         return staticDefinition
       }
     }
@@ -95,5 +97,4 @@ export class StaticTokenDefinition {
     // If not found, return null
     return null
   }
-
 }
